0d6f2b5247c37b2d8f9277cffceaf8345eda40f5
Chao Yu <chao@kernel.org>
2025-04-16 13:48:05 +0800

    f2fs: sysfs: export linear_lookup in features directory
    cat /sys/fs/f2fs/features/linear_lookup
    supported
    
    Signed-off-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>

--- fs/f2fs/sysfs.c
+++ fs/f2fs/sysfs.c
@@ -1586,6 +1586,9 @@ F2FS_RW_ATTR(F2FS_SBI, f2fs_sb_info, compress_percent, compress_percent);
 F2FS_RW_ATTR(F2FS_SBI, f2fs_sb_info, compress_watermark, compress_watermark);
 #endif
 F2FS_FEATURE_RO_ATTR(pin_file);
+#ifdef CONFIG_UNICODE
+F2FS_FEATURE_RO_ATTR(linear_lookup);
+#endif
 
 /* For ATGC */
 F2FS_RW_ATTR(ATGC_INFO, atgc_management, atgc_candidate_ratio, candidate_ratio);
@@ -1775,6 +1778,9 @@ static struct attribute *f2fs_feat_attrs[] = {
 	ATTR_LIST(sec_heimdallfs),
 #endif
 	ATTR_LIST(pin_file),
+#ifdef CONFIG_UNICODE
+	ATTR_LIST(linear_lookup),
+#endif
 	ATTR_LIST(sec_reliable_pinning),
 	NULL,
 };
